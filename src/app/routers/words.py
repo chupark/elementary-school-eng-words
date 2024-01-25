b64ed9826bdaabac201c00b9a1adefb2de7a7da7
chiwoo <chiwoo.park@navercorp.com>
2024-01-25 14:50:59 +0900

    답변 형식 변경

--- src/app/routers/words.py
+++ src/app/routers/words.py
@@ -25,7 +25,7 @@ async def get_nouns(samplings: Annotated[int, Query()],
                     summary="""samplings 수 만큼의 명사(noun)를 랜덤으로 응답합니다."""):
     sql_res = await crud.get_nouns(sampling=samplings)
     print(sql_res)
-    res = {"nouns": sql_res}
+    res = {"명사(nouns)": sql_res}
     return res
 
 @router.get("/pronouns")
@@ -34,7 +34,7 @@ async def get_pronouns(samplings: Annotated[int, Query()],
                         summary="""samplings 수 만큼의 대명사(pronoun)를 랜덤으로 응답합니다."""):
     sql_res = await crud.get_pronouns(sampling=samplings)
     print(sql_res)
-    res = {"pronouns": sql_res}
+    res = {"대명사(pronouns)": sql_res}
     return res
 
 @router.get("/verbs")
@@ -43,7 +43,7 @@ async def get_verbs(samplings: Annotated[int, Query()],
                     summary="""samplings 수 만큼의 동사(verb)를 랜덤으로 응답합니다."""):
     sql_res = await crud.get_verbs(sampling=samplings)
     print(sql_res)
-    res = {"verbs": sql_res}
+    res = {"동사(verbs)": sql_res}
     return res
 
 @router.get("/adjectives")
@@ -52,7 +52,7 @@ async def get_adjectives(samplings: Annotated[int, Query()],
                         summary="""samplings 수 만큼의 형용사(adjective)를 랜덤으로 응답합니다."""):
     sql_res = await crud.get_adjectives(sampling=samplings)
     print(sql_res)
-    res = {"adjectives": sql_res}
+    res = {"형용사(adjectives)": sql_res}
     return res
 
 @router.get("/adverbs")
@@ -61,7 +61,7 @@ async def get_adverbs(samplings: Annotated[int, Query()],
                         summary="""samplings 수 만큼의 부사(adverb)를 랜덤으로 응답합니다."""): 
     sql_res = await crud.get_adverbs(sampling=samplings)
     print(sql_res)
-    res = {"adverbs": sql_res}
+    res = {"부사(adverbs)": sql_res}
     return res
 
 @router.get("/prepositions")
@@ -70,7 +70,7 @@ async def get_prepositions(samplings: Annotated[int, Query()],
                             summary="""samplings 수 만큼의 전치사(preposition)를 랜덤으로 응답합니다."""): 
     sql_res = await crud.get_prepositions(sampling=samplings)
     print(sql_res)
-    res = {"prepositions": sql_res}
+    res = {"전치사(prepositions)": sql_res}
     return res
 
 @router.get("/conjunctions")
@@ -79,7 +79,7 @@ async def get_conjunctions(samplings: Annotated[int, Query()],
                             summary="""samplings 수 만큼의 접속사(conjunction)를 랜덤으로 응답합니다."""): 
     sql_res = await crud.get_conjunctions(sampling=samplings)
     print(sql_res)
-    res = {"conjunctions": sql_res}
+    res = {"접속사(conjunctions)": sql_res}
     return res
 
 @router.get("/interjections")
@@ -88,5 +88,5 @@ async def get_interjections(samplings: Annotated[int, Query()],
                             summary="""samplings 수 만큼의 감탄사(interjection)를 랜덤으로 응답합니다."""): 
     sql_res = await crud.get_interjections(sampling=samplings)
     print(sql_res)
-    res = {"interjections": sql_res}
+    res = {"감탄사(interjections)": sql_res}
     return res
